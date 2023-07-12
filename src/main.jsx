6734e14f5c6c676c3bb196b1e76871923a107d8a
catabsilva <work@catarinasilva.com>
2023-07-12 18:13:29 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Inicio/>}/>
-      <Route path="/Home" element={<App/>}/>
-      <Route path="/Healthy" element={<Healthy/>}/>
-      <Route path="/Unhealthy" element={<Unhealthy/>}/>
-      <Route path="/FazerPedido" element={<Formulario/>}/>
-      <Route path="/GerirPedidos" element={<Pedidos/>}/>
+      <Route path="/unhealthy-restaurant/" element={<Inicio/>}/>
+      <Route path="/unhealthy-restaurant/Home" element={<App/>}/>
+      <Route path="/unhealthy-restaurant/Healthy" element={<Healthy/>}/>
+      <Route path="/unhealthy-restaurant/Unhealthy" element={<Unhealthy/>}/>
+      <Route path="/unhealthy-restaurant/FazerPedido" element={<Formulario/>}/>
+      <Route path="/unhealthy-restaurant/GerirPedidos" element={<Pedidos/>}/>
     </Routes>
   </BrowserRouter>
   </React.StrictMode>,
