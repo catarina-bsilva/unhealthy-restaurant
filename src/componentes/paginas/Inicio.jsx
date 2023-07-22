5084549abf04e2c6fc0e74b26e68d6fd0a96f819
catabsilva <work@catarinasilva.com>
2023-07-22 18:15:51 +0200

    updatting the project

--- src/componentes/paginas/Inicio.jsx
+++ src/componentes/paginas/Inicio.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
-
+import logo from '../../../dist/img/logotipo.png'
 import '../../styles/inicio.sass'
 
 const Inicio = () => {
   return (
     <div id='Inicio'>
-        <Link to="/unhealthy-restaurant/Home"><img src="/img/logotipo.png" alt="Logotipo" /></Link>
+        <Link to="/unhealthy-restaurant/Home"><img src={logo} alt="Logotipo" /></Link>
     </div>
   )
 }
